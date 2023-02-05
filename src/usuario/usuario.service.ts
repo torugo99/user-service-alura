4d049e1ddfc218aa149ca1e284a169d68a3ea777
Victor Hugo <victormotabatista@gmail.com>
2023-02-05 15:56:32 -0300

    service commit

---
+++ src/usuario/usuario.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@nestjs/common';
+import { Usuario } from './usuario.entity';
+
+@Injectable()
+export class UsuarioService {
+    private usuarios: Array<Usuario> = [{ 
+        id: 1,
+        nomeDeUsuario: 'victor99dev',
+        email: 'victor99dev@teste.com.br',
+        senha: '123456',
+        nomeCompleto: 'Victor99dev',
+        dataDeEntrada: new Date()
+    }];
+    
+    public cria(usuario: Usuario): Usuario {
+        this.usuarios.push(usuario);
+        
+        return usuario;
+    }
+
+    public buscaPorNomeDeUsuario(nomeDeUsuario: string): Usuario {
+        const usuarioEncontrado = this.usuarios.find(usuario => usuario.nomeDeUsuario == nomeDeUsuario);
+
+        return usuarioEncontrado;
+    }
+}
